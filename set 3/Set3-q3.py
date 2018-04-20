2a1fbffa34cd9de1e1a991f3ff260bf54316a42a
rauni09 <rauni.bitw@gmail.com>
2018-04-20 23:53:28 +0530

    secondary commit

--- set 3/Set3-q3.py
+++
@@ -1,9 +0,0 @@
-
-reversedNumber=0
-n=int(input("\nEnter an integer : "))
-while n!=0:
-  remainder=n%10
-  reversedNumber=reversedNumber*10+remainder
-  n//=10
-print("Reversed Number  : %d\n"%reversedNumber)  
-
